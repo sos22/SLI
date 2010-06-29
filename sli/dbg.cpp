4420fa7621c9e88cadb44e55706d0745035a531e
Steven Smith <sos22@cl.cam.ac.uk>
2010-06-29 16:46:46 +0100

    Make it a bit easier to attach gdb to a pickled machine state.

---
+++ sli/dbg.cpp
@@ -0,0 +1,23 @@
+#include <err.h>
+#include "sli.h"
+
+int
+main(int argc, char *argv[])
+{
+	init_sli();
+
+	LogFile *lf;
+	LogReaderPtr ptr;
+
+	lf = LogFile::open(argv[1], &ptr);
+	if (!lf)
+		err(1, "opening %s", argv[1]);
+	VexGcRoot((void **)&lf, "lf");
+
+	MachineState<unsigned long> *ms_base = MachineState<unsigned long>::initialMachineState(lf, ptr, &ptr);
+	VexGcRoot((void **)&ms_base, "ms_base");
+
+	gdb_concrete(ms_base);
+
+	return 0;
+}
