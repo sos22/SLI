cb826cff628634342c84e0cf3d8f07130db69b2f
Steven Smith <sos22@cl.cam.ac.uk>
2011-05-26 18:43:56 +0100

    Add a standalone test program for the bits which are supposed to discover remote macro sections.

---
+++ tests/sli/find_remote_sections.cpp
@@ -0,0 +1,42 @@
+#include <sys/fcntl.h>
+#include <unistd.h>
+
+#include "sli.h"
+#include "oracle.hpp"
+#include "state_machine.hpp"
+#include "eval_state_machine.hpp"
+
+int
+main(int argc, char *argv[])
+{
+	init_sli();
+
+	VexPtr<Oracle> oracle(new Oracle(NULL, NULL, NULL));
+
+	VexPtr<StateMachine, &ir_heap> readMachine(readStateMachine(open(argv[1], O_RDONLY)));
+	VexPtr<StateMachine, &ir_heap> writeMachine(readStateMachine(open(argv[2], O_RDONLY)));
+	VexPtr<IRExpr, &ir_heap> assumption(readIRExpr(open(argv[3], O_RDONLY)));
+	
+	remoteMacroSectionsT remoteMacroSections;
+
+	if (!findRemoteMacroSections(readMachine, writeMachine, assumption, oracle, remoteMacroSections)) {
+		printf("Cannot find remote macro sections...\n");
+		return 1;
+	}
+	for (remoteMacroSectionsT::iterator it = remoteMacroSections.begin();
+	     it != remoteMacroSections.end();
+	     it++) {
+		printf("\t\tRemote macro section ");
+		it->first->prettyPrint(stdout);
+		printf(" -> ");
+		it->second->prettyPrint(stdout);
+		printf("\n");		
+	}
+	if (!fixSufficient(readMachine, writeMachine, assumption, oracle, remoteMacroSections)) {
+		printf("Fix insufficient\n");
+		return 1;
+	} else {
+		printf("Fix sufficient\n");
+		return 0;
+	}
+}
