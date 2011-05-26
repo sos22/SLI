18cbdfbad8fcde0c4c3269d2e5d59ca02779e227
Steven Smith <sos22@cl.cam.ac.uk>
2011-05-26 17:19:08 +0100

    Add a thing for evaluating cross product machines by themselves.

---
+++ tests/sli/eval_cross_product.cpp
@@ -0,0 +1,29 @@
+#include <sys/fcntl.h>
+#include <unistd.h>
+
+#include "sli.h"
+#include "oracle.hpp"
+#include "state_machine.hpp"
+#include "eval_state_machine.hpp"
+
+int
+main(int argc, char *argv[])
+{
+	init_sli();
+
+	VexPtr<Oracle> oracle(new Oracle(NULL, NULL, NULL));
+
+	VexPtr<StateMachine, &ir_heap> readMachine(readStateMachine(open(argv[1], O_RDONLY)));
+	VexPtr<StateMachine, &ir_heap> writeMachine(readStateMachine(open(argv[2], O_RDONLY)));
+	VexPtr<IRExpr, &ir_heap> assumption(readIRExpr(open(argv[3], O_RDONLY)));
+	
+	bool mightCrash;
+	bool mightSurvive;
+
+	evalCrossProductMachine(readMachine, writeMachine, oracle, assumption,
+				&mightSurvive, &mightCrash, ALLOW_GC);
+	printf("Might survive %d, might crash %d\n",
+	       mightSurvive, mightCrash);
+
+	return 0;
+}
