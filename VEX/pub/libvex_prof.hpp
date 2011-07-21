ec77a926b229c30883648d784b4d70a3ac8900b4
Steven Smith <sos22@cam.ac.uk>
2011-07-21 15:42:09 +0100

    Make __set_profiling safe in recursive functions.

--- VEX/pub/libvex_prof.hpp
+++ VEX/pub/libvex_prof.hpp
@@ -8,9 +8,10 @@ public:
 	unsigned long accumulated_ticks;
 	unsigned long nr_sites;
 	ProfilingSite *next;
+	bool live;
 	static ProfilingSite *head_prof_site;
 	ProfilingSite(const char *_name)
-		: name(_name), accumulated_ticks(0), nr_sites(0)
+		: name(_name), accumulated_ticks(0), nr_sites(0), live(false)
 	{
 		next = head_prof_site;
 		head_prof_site = this;
@@ -27,11 +28,26 @@ public:
 	}
 	ProfilingSite *site;
 	unsigned long start;
+	bool doit;
 	SetProfiling(ProfilingSite *_site)
-		: site(_site), start(rdtsc())
-	{}
+		: site(_site)
+	{
+		doit = !site->live;
+		if (doit) {
+			site->live = true;
+			start = rdtsc();
+		} else {
+			start = 0;
+		}
+	}
 	~SetProfiling()
-	{ site->accumulated_ticks += rdtsc() - start; site->nr_sites++; }
+	{
+		if (doit) {
+			site->accumulated_ticks += rdtsc() - start;
+			site->nr_sites++;
+			site->live = false;
+		}
+	}
 };
 
 #define __set_profiling(name)						\
