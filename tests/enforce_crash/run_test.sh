f00f64d0d5c3af0859f54427b8f34c090cade680
Steven Smith <sos22@cam.ac.uk>
2011-08-04 13:43:53 +0100

    Add a simpler test case for enforce_crash, and a script to drive it more easily.

---
+++ tests/enforce_crash/run_test.sh
@@ -0,0 +1,43 @@
+#! /bin/sh
+
+set -e
+
+make $1
+
+t=../$1
+shift
+args="$*"
+
+rm -rf res
+mkdir res
+cd res
+
+objdump -d "$t" > disassembly
+
+# First, collect a types table
+../../valgrind-ft-stage3/bin/valgrind --tool=ft2 $t $args
+
+# And a BCG table
+../../valgrind-ft-stage3/bin/valgrind --tool=bcg $t $args
+
+echo Done BCG
+
+# Now run minimal_direct to build the crash summaries
+mkdir crash_summaries
+mkdir logs
+../minimal_direct $t types1.dat callgraph1.dat
+
+# Classify the logs
+cd logs
+for x in *
+do
+    ../../scripts/classify_split_typescript.sh $x
+done
+cd results
+summarise_directory.sh
+
+cd ../../
+for x in crash_summaries/*
+do
+    ../enforce_crash $t types1.dat callgraph1.dat $x
+done
---
+++ tests/enforce_crash/test1.c
@@ -0,0 +1,89 @@
+/* A simple test program which crashes after a while.  enforce_crash
+   makes it crash just that little bit sooner. */
+#include <assert.h>
+#include <err.h>
+#include <pthread.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+static long max_iterations;
+static volatile long nr_iterations;
+static int *volatile the_ptr;
+
+static void
+segv_handler(int ignore)
+{
+	/* Can't use printf, because we're in a signal handler, but
+	   assembling it manually and then using write() directly is
+	   allowed. */
+	char buf[] = "000000000000000000000 iterations\n";
+	int cntr = nr_iterations;
+	int i = 19;
+	ssize_t s;
+
+	while (cntr) {
+		assert(i >= 0);
+		buf[i] = '0' + (cntr % 10);
+		cntr /= 10;
+		i--;
+	}
+	s = write(1, buf + i + 1, sizeof(buf) - 2 - i);
+	(void)s;
+
+	_exit(0);
+}
+
+static void *
+read_thread(void *ignore)
+{
+	while (max_iterations == -1 || nr_iterations < max_iterations) {
+		if (the_ptr != NULL)
+			(*the_ptr)++;
+		nr_iterations++;
+	}
+	return NULL;
+}
+
+int
+main(int argc, char *argv[])
+{
+	pthread_t t;
+	static int my_int;
+	double delay;
+
+	if (argc < 2 || argc > 3) {
+		fprintf(stderr,
+"%s <delay> {<end>}\n"
+"<delay> is in seconds, as a float, and up to 2\n"
+"<end>, if present, is the number of iterations to run before giving up and reporting success\n",
+			argv[0]);
+		return 1;
+	}
+
+	delay = atof(argv[1]);
+	printf("Delay is %f\n", delay);
+	if (delay > 2)
+		errx(1, "delay must be <= 2 seconds");
+	max_iterations = -1;
+	if (argc == 3) {
+		max_iterations = atol(argv[2]);
+		printf("Max iterations %ld\n", max_iterations);
+	}
+	signal(SIGSEGV, segv_handler);
+
+	my_int = 0;
+	the_ptr = &my_int;
+
+	if (pthread_create(&t, NULL, read_thread, NULL) != 0)
+		errx(1, "spawning read thread");
+
+	while (max_iterations == -1 || nr_iterations < max_iterations) {
+		usleep(delay * 1e6);
+		the_ptr = NULL;
+		usleep(delay * 1e6);
+		the_ptr = &my_int;
+	}
+	return 0;
+}
