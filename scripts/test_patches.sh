90af2e96b9120cca72cf22149e3a8b8bd0a908d7
Steven Smith <sos22@cam.ac.uk>
2012-01-31 18:03:30 +0000

    De-moronise test_patches.sh a little bit.

--- scripts/test_patches.sh
+++ scripts/test_patches.sh
@@ -6,7 +6,7 @@ then
     exit 1
 fi
 
-TOP=${0%test_ceds.sh}
+TOP=${0%test_patches.sh}
 
 echo "topdir $TOP"
 
@@ -22,7 +22,7 @@ mkdir patch_sources
 mkdir cores
 
 rm enforce_crash_*
-for crash_summary in crash_summaries/*
+for crash_summary in crash_summaries/684 crash_summaries/*
 do
     echo -n "Processing $crash_summary at "
     date +%s
@@ -40,6 +40,7 @@ do
     echo -n "Start generating CED at "
     date +%s
 
+    echo "crash summary $crash_summary"
     script -f -c "../enforce_crash mysqld types1.dat callgraph1.dat ${ced} ${crash_summary}" logs/enforce_crash${id}.scr || continue
 
     echo -n "Start generating patch at "
@@ -48,13 +49,14 @@ do
     script -f -c "../ced_to_patch mysqld ${ced} ${patch}" logs/ced_to_patch${id}.scr || continue
     mv enforce_crash_* patch_sources/${id}.c
 
-    echo -n "Start testing patch at "
+    echo -n "Start testing patch ${patch} at "
     date +%s
 
     export SOS22_ENFORCER_MAX_STALLS=10000
+    export SOS22_ENFORCER_PATH=${topdir}/${patch}
     cd /local/scratch/sos22/mysql-5.5.6-rc/mysql-test
     rm -rf var
-    script -f -c "./mtr --testcase-timeout=99999 --max-test-fail=1 rpl_change_master" ${topdir}/logs/mtr${id}.scr
+    script -f -c "echo patch \$SOS22_ENFORCER_PATH; ./mtr --testcase-timeout=99999 --max-test-fail=1 rpl_change_master" ${topdir}/logs/mtr${id}.scr
 
     echo -n "Finished test harness at "
     date +%s
