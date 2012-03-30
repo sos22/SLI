f7cb3328823ccf455d7ac0611d0f8ef54cd26ce8
Steven Smith <sos22@cam.ac.uk>
2012-03-30 12:02:35 +0100

    Add a new mode to minimal_direct, in which it processes some sub-range of instructions specified by start and end percentages.  This is useful for parallelised runs.

--- sli/minimal_direct.cpp
+++ sli/minimal_direct.cpp
@@ -50,6 +50,7 @@ DumpFix::operator()(VexPtr<CrashSummary, &ir_heap> &summary,
 	} while (fd == -1 && errno == EEXIST);
 	if (fd == -1)
 		err(1, "opening %s", buf);
+	fprintf(_logfile, "Write summary to %s\n", buf);
 	free(buf);
 	FILE *f = fdopen(fd, "w");
 	if (!f)
@@ -173,86 +174,114 @@ main(int argc, char *argv[])
 
 	LibVEX_gc(ALLOW_GC);
 
+	int start_percentage;
+	int end_percentage;
+
+	start_percentage = 0;
+	end_percentage = 100;
+
 	if (argc == 5) {
 		VexRip vr;
 		const char *succ;
-		if (!parseVexRip(&vr, argv[4], &succ))
-			errx(1, "cannot parse %s as VexRip", argv[4]);
-		consider_rip(vr, ms, thr, oracle, df, NULL, ALLOW_GC);
-	} else {
-		FILE *timings = fopen("timings.txt", "w");
-		VexPtr<TypesDb::all_instrs_iterator> instrIterator(oracle->type_index->enumerateAllInstructions());
-		unsigned long total_instructions = oracle->type_index->nrDistinctInstructions();
-		unsigned long instructions_processed = 0;
-		printf("%ld instructions to protect\n", total_instructions);
-		double start = now();
-		double low_end_time;
-		double high_end_time;
-		bool first = true;
-		int cntr = 0;
-		while (!instrIterator->finished()) {
-			DynAnalysisRip dar;
-			instrIterator->fetch(&dar);
-			_logfile = fopenf("w", "logs/%d", cntr);
-			if (!_logfile) err(1, "opening logs/%d", cntr);
-			VexRip rip = dar.toVexRip();
-			printf("Considering %s, log logs/%d\n", rip.name(), cntr);
-			fprintf(_logfile, "Log for %s:\n", rip.name());
-			cntr++;
-			consider_rip(rip, ms, thr, oracle, df, timings, ALLOW_GC);
-			fclose(_logfile);
-			_logfile = stdout;
-
-			instrIterator->advance();
-			instructions_processed++;
-
-			double completion = instructions_processed / double(total_instructions);
-			double elapsed = now() - start;
-			double total_estimated = elapsed / completion;
-			double endd = total_estimated + start;
-			if (isinf(endd))
-				continue;
-
-			dump_profiling_data();
-
-			time_t end = endd;
-			char *times;
-			if (first) {
-				low_end_time = endd;
-				high_end_time = endd;
-				first = false;
-				times = my_asprintf("finish at %s", ctime(&end));
-			} else {
-				low_end_time = low_end_time * .99 + endd * 0.01;
-				high_end_time = high_end_time * .99 + endd * 0.01;
-				if (low_end_time > endd)
-					low_end_time = endd;
-				if (high_end_time < endd)
-					high_end_time = endd;
-				char *t = strdup(ctime(&end));
-				t[strlen(t)-1] = 0;
-				end = low_end_time;
-				char *t2 = strdup(ctime(&end));
-				t2[strlen(t2)-1] = 0;
-				end = high_end_time;
-				char *t3 = strdup(ctime(&end));
-				t3[strlen(t3)-1] = 0;
-				times = my_asprintf("finish at %s [%s,%s]\n",
-						    t, t2, t3);
-				free(t);
-				free(t2);
-				free(t3);
-			}
-			printf("Done %ld/%ld(%f%%) in %f seconds (%f each); %f left; %s",
-			       instructions_processed,
-			       total_instructions,
-			       completion * 100,
-			       elapsed,
-			       elapsed / instructions_processed,
-			       total_estimated - elapsed,
-			       times);
-			free(times);
+		if (parseVexRip(&vr, argv[4], &succ)) {
+			consider_rip(vr, ms, thr, oracle, df, NULL, ALLOW_GC);
+			df.finish();
+			return 0;
 		}
+		if (sscanf(argv[4], "%d...%d", &start_percentage, &end_percentage) != 2)
+			errx(1, "expect final argument to be either a VexRip or s...d where s and d are start and end percentages");
+	}
+
+	FILE *timings = fopen("timings.txt", "w");
+	VexPtr<TypesDb::all_instrs_iterator> instrIterator(oracle->type_index->enumerateAllInstructions());
+	unsigned long total_instructions = oracle->type_index->nrDistinctInstructions();
+	unsigned long instructions_processed = 0;
+	printf("%ld instructions to protect\n", total_instructions);
+
+	/* There are a couple of important properties here:
+
+	   -- 0...100 must precisely cover the entire range
+	   -- a...b and b...c must, between them, cover precisely the
+              same range as a...c i.e. no duplicates or gaps.
+	*/
+	unsigned long start_instr = total_instructions / 100 * start_percentage;
+	unsigned long end_instr = end_percentage == 100 ? total_instructions : total_instructions / 100 * end_percentage - 1;
+	unsigned long instructions_to_process = end_instr - start_instr + 1;
+
+	printf("Processing instructions %ld to %ld\n", start_instr, end_instr);
+
+	/* Skip the ones we've been told to skip. */
+	for (unsigned long a = 0; a < start_instr; a++)
+		instrIterator->advance();
+
+	double start = now();
+	double low_end_time;
+	double high_end_time;
+	bool first = true;
+	unsigned long cntr = 0;
+	while (cntr < instructions_to_process) {
+		assert(!instrIterator->finished());
+		DynAnalysisRip dar;
+		instrIterator->fetch(&dar);
+		_logfile = fopenf("w", "logs/%ld", cntr + start_instr);
+		if (!_logfile) err(1, "opening logs/%ld", cntr + start_instr);
+		VexRip rip = dar.toVexRip();
+		printf("Considering %s, log logs/%ld\n", rip.name(), cntr + start_instr);
+		fprintf(_logfile, "Log for %s:\n", rip.name());
+		cntr++;
+		consider_rip(rip, ms, thr, oracle, df, timings, ALLOW_GC);
+		fclose(_logfile);
+		_logfile = stdout;
+
+		instrIterator->advance();
+		instructions_processed++;
+
+		double completion = instructions_processed / double(instructions_to_process);
+		double elapsed = now() - start;
+		double total_estimated = elapsed / completion;
+		double endd = total_estimated + start;
+		if (isinf(endd))
+			continue;
+
+		dump_profiling_data();
+
+		time_t end = endd;
+		char *times;
+		if (first) {
+			low_end_time = endd;
+			high_end_time = endd;
+			first = false;
+			times = my_asprintf("finish at %s", ctime(&end));
+		} else {
+			low_end_time = low_end_time * .99 + endd * 0.01;
+			high_end_time = high_end_time * .99 + endd * 0.01;
+			if (low_end_time > endd)
+				low_end_time = endd;
+			if (high_end_time < endd)
+				high_end_time = endd;
+			char *t = strdup(ctime(&end));
+			t[strlen(t)-1] = 0;
+			end = low_end_time;
+			char *t2 = strdup(ctime(&end));
+			t2[strlen(t2)-1] = 0;
+			end = high_end_time;
+			char *t3 = strdup(ctime(&end));
+			t3[strlen(t3)-1] = 0;
+			times = my_asprintf("finish at %s [%s,%s]\n",
+					    t, t2, t3);
+			free(t);
+			free(t2);
+			free(t3);
+		}
+		printf("Done %ld/%ld(%f%%) in %f seconds (%f each); %f left; %s",
+		       instructions_processed,
+		       total_instructions,
+		       completion * 100,
+		       elapsed,
+		       elapsed / instructions_processed,
+		       total_estimated - elapsed,
+		       times);
+		free(times);
 	}
 
 	df.finish();
