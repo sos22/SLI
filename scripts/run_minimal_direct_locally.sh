2c7af371bfcc2b78d41f738f8963ea91eb96b0f4
Steven Smith <sos22@cam.ac.uk>
2012-05-16 09:19:08 +0100

    Tidy up script a little.

--- scripts/run_minimal_direct_locally.sh
+++ scripts/run_minimal_direct_locally.sh
@@ -3,5 +3,10 @@
 start="$1"
 end="$2"
 
-cd /local/scratch/sos22/sli/2012-04-04b
-./minimal_direct ./mysqld types-new-format-no-head-canon.dat callgraph-newformat.dat ${start}...${end}
+set -e
+set -x
+
+ulimit -c unlimited
+
+cd /local/scratch/sos22/sli/2012-05-15
+../minimal_direct ./mysqld types-new-format-no-head-canon.dat callgraph-newformat.dat ${start}...${end}
