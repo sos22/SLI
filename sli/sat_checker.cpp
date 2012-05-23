0913ca374c4a12b4192bb4d5d4d3eb434048f0a7
Steven Smith <sos22@cam.ac.uk>
2012-05-23 18:01:00 +0100

    Better conversion to and from CNF and DNF in the satisfiability checker.

--- sli/sat_checker.cpp
+++ sli/sat_checker.cpp
@@ -542,8 +542,11 @@ conjunctive_normal_form(IRExpr *what)
 {
 	NF_Expression ne;
 	Maybe<bool> res = convert_to_nf(what, ne, Iop_And1, Iop_Or1);
-	if (res == Maybe<bool>::just(true))
+	if (res == Maybe<bool>::just(true)) {
+		if (ne.size() == 0)
+			return IRExpr_Const(IRConst_U1(1));
 		return convert_from_nf(ne, Iop_And1, Iop_Or1);
+	}
 	if (res == Maybe<bool>::just(false))
 		return IRExpr_Const(IRConst_U1(0));
 	return what;
@@ -554,8 +557,11 @@ disjunctive_normal_form(IRExpr *what)
 {
 	NF_Expression ne;
 	Maybe<bool> res = convert_to_nf(what, ne, Iop_Or1, Iop_And1);
-	if (res == Maybe<bool>::just(true))
+	if (res == Maybe<bool>::just(true)) {
+		if (ne.size() == 0)
+			return IRExpr_Const(IRConst_U1(0));
 		return convert_from_nf(ne, Iop_Or1, Iop_And1);
+	}
 	if (res == Maybe<bool>::just(false))
 		return IRExpr_Const(IRConst_U1(1));
 	return what;
