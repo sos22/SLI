0445c72361de74cabcc346e052b81883b5ea4dad
Steven Smith <sos22@cam.ac.uk>
2012-06-18 10:54:51 +0100

    Crash summaries are only really interesting when the verification condition is true.  That allows us to make some additional simplifications during canonicalisation.

--- sli/canonicalise_crash_summary2.cpp
+++ sli/canonicalise_crash_summary2.cpp
@@ -6,6 +6,7 @@
 #include "intern.hpp"
 #include "allowable_optimisations.hpp"
 #include "offline_analysis.hpp"
+#include "nf.hpp"
 
 static StateMachine *
 removeMarkers(VexPtr<StateMachine, &ir_heap> sm,
@@ -25,15 +26,76 @@ removeMarkers(VexPtr<StateMachine, &ir_heap> sm,
 	return optimiseStateMachine(sm, opt, oracle, false, token);
 }
 
+static StateMachine *
+optimiseStateMachineAssuming(StateMachine *sm,
+			     IRExpr *assumption,
+			     bool assumptionIsTrue)
+{
+	assert(assumption->type() == Ity_I1);
+	if (assumption->tag == Iex_Associative) {
+		IRExprAssociative *a = (IRExprAssociative *)assumption;
+		if ( (assumptionIsTrue && a->op == Iop_And1) ||
+		     (!assumptionIsTrue && a->op == Iop_Or1) ) {
+			for (int i = 0; i < a->nr_arguments; i++)
+				sm = optimiseStateMachineAssuming(sm, a->contents[i],
+								  assumptionIsTrue);
+			return sm;
+		}
+	}
+	if (assumption->tag == Iex_Unop) {
+		IRExprUnop *a = (IRExprUnop *)assumption;
+		if (a->op == Iop_Not1)
+			return optimiseStateMachineAssuming(sm, a->arg,
+							    !assumptionIsTrue);
+	}
+
+	struct : public StateMachineTransformer {
+		IRExpr *assumption;
+		bool assumptionIsTrue;
+
+		IRExpr *transformIRExpr(IRExpr *e, bool *done_something) {
+			if (e == assumption) {
+				*done_something = true;
+				return IRExpr_Const(IRConst_U1(assumptionIsTrue));
+			}
+			return StateMachineTransformer::transformIRExpr(e, done_something);
+		}
+		bool rewriteNewStates() const { return false; }
+	} doit;
+	doit.assumption = assumption;
+	doit.assumptionIsTrue = assumptionIsTrue;
+	return doit.transform(sm);
+}
+
 static CrashSummary *
 canonicalise_crash_summary(VexPtr<CrashSummary, &ir_heap> input,
 			   VexPtr<Oracle> oracle,
 			   const AllowableOptimisations &optIn,
 			   GarbageCollectionToken token)
 {
-	VexPtr<StateMachine, &ir_heap> sm(input->loadMachine);
+	VexPtr<StateMachine, &ir_heap> sm;
+	{
+		IRExpr * cnf_condition;
+		cnf_condition = convert_to_cnf(input->verificationCondition);
+		if (!cnf_condition)
+			cnf_condition = input->verificationCondition;
+
+		internStateMachineTable intern;
+		input->loadMachine = internStateMachine(input->loadMachine, intern);
+		input->storeMachine = internStateMachine(input->storeMachine, intern);
+		input->verificationCondition = internIRExpr(input->verificationCondition, intern);
+
+		cnf_condition = internIRExpr(cnf_condition, intern);
+		input->loadMachine = optimiseStateMachineAssuming(input->loadMachine, cnf_condition,
+								  true);
+		input->storeMachine = optimiseStateMachineAssuming(input->storeMachine, cnf_condition,
+								   true);
+	}
+
+	sm = input->loadMachine;
 	input->loadMachine = removeMarkers(sm, optIn, oracle, token);
 	input->loadMachine = removeAssertions(input->loadMachine, optIn, oracle, false, token);
+
 	sm = input->storeMachine;
 	input->storeMachine = removeMarkers(sm, optIn, oracle, token);
 	input->storeMachine = removeAssertions(input->storeMachine, optIn, oracle, false, token);
