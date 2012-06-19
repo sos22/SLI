1fb7e15e46616c10475c1c722f02b4820287e2dd
Steven Smith <sos22@cam.ac.uk>
2012-06-19 16:50:09 +0100

    Marginal improvement to phase 3 canonicaliser: remove clauses which are underspecified by the structure of the verification condition.

--- sli/canonicalise_crash_summary3.cpp
+++ sli/canonicalise_crash_summary3.cpp
@@ -134,10 +134,30 @@ operator -=(std::set<t, comp> &out, const std::set<t, comp> &a)
 		out.erase(*it);
 }
 
+static int
+findRegisterMultiplicity(const IRExpr *iex, const threadAndRegister &r)
+{
+	struct _ : public IRExprTransformer {
+		const threadAndRegister &r;
+		int multiplicity;
+		IRExpr *transformIex(IRExprGet *ieg) {
+			if (threadAndRegister::fullEq(ieg->reg, r))
+				multiplicity++;
+			return ieg;
+		}
+		_(const threadAndRegister &_r)
+			: r(_r), multiplicity(0)
+		{}
+	} doit(r);
+	doit.doit(const_cast<IRExpr *>(iex));
+	return doit.multiplicity;
+}
+
 static IRExpr *
 removeRedundantClauses(IRExpr *verificationCondition,
 		       internIRExprTable &intern,
-		       const reg_set_t &targetRegisters)
+		       const reg_set_t &targetRegisters,
+		       bool *done_something)
 {
 	verificationCondition = simplify_via_anf(verificationCondition);
 	verificationCondition = convert_to_cnf(verificationCondition);
@@ -189,6 +209,9 @@ removeRedundantClauses(IRExpr *verificationCondition,
 	}
 #endif
 
+	if (nr_kept != nr_verification_clauses)
+		*done_something = true;
+
 	if (nr_kept == 0) {
 		return IRExpr_Const(IRConst_U1(1));
 	} else if (nr_kept == 1) {
@@ -207,6 +230,162 @@ removeRedundantClauses(IRExpr *verificationCondition,
 	}
 }
 
+/* Returns true if the expression could be anything at all.  The idea
+ * is that if a variable only appears once in the verification
+ * condition, and the operators used are appropriate, we can make the
+ * value of the expression be anything we want by setting the variable
+ * as appropriate.  If a clause in the verification condition is
+ * underspecified in this way then it's not actually telling us
+ * anything, and so we might as well remove it. */
+static bool
+clauseUnderspecified(IRExpr *clause,
+		     const std::map<threadAndRegister, int, threadAndRegister::fullCompare> &mult)
+{
+	switch (clause->tag) {
+	case Iex_Unop: {
+		IRExprUnop *ieu = (IRExprUnop *)clause;
+		switch (ieu->op) {
+		case Iop_Not1:
+		case Iop_Not8:
+		case Iop_Not16:
+		case Iop_Not32:
+		case Iop_Not64:
+		case Iop_Neg8:
+		case Iop_Neg16:
+		case Iop_Neg32:
+		case Iop_Neg64:
+		case Iop_BadPtr:
+			return clauseUnderspecified(ieu->arg, mult);
+		case Iop_32Uto64:
+			return false;
+		default:
+			abort();
+		}
+	}
+	case Iex_Binop: {
+		IRExprBinop *ieb = (IRExprBinop *)clause;
+		switch (ieb->op) {
+		case Iop_CmpEQ8:
+		case Iop_CmpEQ16:
+		case Iop_CmpEQ32:
+		case Iop_CmpEQ64:
+		case Iop_CmpLT8U:
+		case Iop_CmpLT16U:
+		case Iop_CmpLT32U:
+		case Iop_CmpLT64U:
+			return clauseUnderspecified(ieb->arg1, mult) ||
+				clauseUnderspecified(ieb->arg2, mult);
+		case Iop_Shl64:
+			return clauseUnderspecified(ieb->arg1, mult) &&
+				clauseUnderspecified(ieb->arg2, mult);
+		default:
+			abort();
+		}
+	}
+	case Iex_Associative: {
+		IRExprAssociative *iea = (IRExprAssociative *)clause;
+		bool acc;
+		if (iea->nr_arguments == 0)
+			return false;
+		switch (iea->op) {
+		case Iop_Add8:
+		case Iop_Add16:
+		case Iop_Add32:
+		case Iop_Add64:
+			acc = false;
+			for (int i = 0; i < iea->nr_arguments; i++)
+				acc |= clauseUnderspecified(iea->contents[i], mult);
+			break;
+		case Iop_And1:
+		case Iop_And8:
+		case Iop_And16:
+		case Iop_And32:
+		case Iop_And64:
+		case Iop_Or1:
+		case Iop_Or8:
+		case Iop_Or16:
+		case Iop_Or32:
+		case Iop_Or64:
+			acc = true;
+			for (int i = 0; i < iea->nr_arguments; i++)
+				acc &= clauseUnderspecified(iea->contents[i], mult);
+			break;
+		default:
+			abort();
+		}
+		return acc;
+	}
+	case Iex_Get: {
+		IRExprGet *ieg = (IRExprGet *)clause;
+		auto it = mult.find(ieg->reg);
+		assert(it != mult.end());
+		assert(it->second != 0);
+		return it->second == 1;
+	}
+	default:
+		return false;
+	}
+	abort();
+}
+
+static IRExpr *
+removeUnderspecifiedClauses(IRExpr *input,
+			    internIRExprTable &intern,
+			    const reg_set_t &targetRegisters,
+			    bool *done_something)
+{
+	std::map<threadAndRegister, int, threadAndRegister::fullCompare> mult;
+	reg_set_t allRegisters;
+	enumRegisters(input, &allRegisters);
+	for (auto it = allRegisters.begin(); it != allRegisters.end(); it++)
+		mult[*it] = findRegisterMultiplicity(input, *it);
+	for (auto it = targetRegisters.begin(); it != targetRegisters.end(); it++)
+		mult[*it]++;
+
+	int nr_clauses;
+	IRExpr **clauses;
+	if (input->tag == Iex_Associative &&
+	    ((IRExprAssociative *)input)->op == Iop_And1) {
+		nr_clauses = ((IRExprAssociative *)input)->nr_arguments;
+		clauses = ((IRExprAssociative *)input)->contents;
+	} else {
+		nr_clauses = 1;
+		clauses = &input;
+	}
+
+	int nr_killed = 0;
+	for (int i = 0; i < nr_clauses; i++) {
+		if (clauseUnderspecified(clauses[i], mult)) {
+			printf("Kill underspecified clause ");
+			ppIRExpr(clauses[i], stdout);
+			printf("\n");
+			clauses[i] = NULL;
+			nr_killed++;
+		}
+	}
+	if (nr_killed == 0)
+		return input;
+	*done_something = true;
+	if (nr_killed == nr_clauses)
+		return IRExpr_Const(IRConst_U1(1));
+	assert(nr_clauses != 1);
+	if (nr_killed == nr_clauses - 1) {
+		for (int i = 0; i < nr_clauses; i++)
+			if (clauses[i])
+				return clauses[i];
+		abort();
+	}
+	int i = 0;
+	int j = 0;
+	while (j < nr_clauses) {
+		while (j < nr_clauses && !clauses[j])
+			j++;
+		clauses[i++] = clauses[j++];
+	}
+	((IRExprAssociative *)input)->nr_arguments = i;
+	return internIRExpr(input, intern);
+}
+	
 static bool
 findTargetRegisters(CrashSummary *summary,
 		    OracleInterface *oracle,
@@ -256,10 +435,23 @@ main(int argc, char *argv[])
 	internIRExprTable intern;
 	std::set<threadAndRegister, threadAndRegister::fullCompare> targetRegisters;
 	if (findTargetRegisters(summary, oracle, intern, &targetRegisters)) {
-		summary->verificationCondition =
-			removeRedundantClauses(summary->verificationCondition,
-					       intern,
-					       targetRegisters);
+		bool progress;
+		progress = true;
+		while (progress) {
+			progress = false;
+			summary->verificationCondition =
+				removeRedundantClauses(
+					summary->verificationCondition,
+					intern,
+					targetRegisters,
+					&progress);
+			summary->verificationCondition =
+				removeUnderspecifiedClauses(
+					summary->verificationCondition,
+					intern,
+					targetRegisters,
+					&progress);
+		}
 	}
 
 	if (TIMEOUT)
