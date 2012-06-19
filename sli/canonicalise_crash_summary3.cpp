7036d27479f356346c639b45d36e4a159353d4ca
Steven Smith <sos22@cam.ac.uk>
2012-06-19 15:09:15 +0100

    Split out phase 3 canonicalisation a little bit.

--- sli/canonicalise_crash_summary3.cpp
+++ sli/canonicalise_crash_summary3.cpp
@@ -124,52 +124,32 @@ operator |=(std::set<t, comp> &out, const std::set<t, comp> &a)
 		it1 = out.insert(it1, *it2);
 }
 
-static void
-removeRedundantClauses(CrashSummary *summary, OracleInterface *oracle)
+static IRExpr *
+removeRedundantClauses(IRExpr *verificationCondition,
+		       internIRExprTable &intern,
+		       const std::set<threadAndRegister, threadAndRegister::fullCompare> &targetRegisters)
 {
-	IRExpr *reducedSurvivalConstraint =
-		crossProductSurvivalConstraint(
-			summary->loadMachine,
-			summary->storeMachine,
-			oracle,
-			IRExpr_Const(IRConst_U1(1)),
-			AllowableOptimisations::defaultOptimisations,
-			ALLOW_GC);
-
-	if (!reducedSurvivalConstraint) {
-		fprintf(stderr, "can't build cross product survival constraint\n");
-		return;
-	}
-
-	reducedSurvivalConstraint = simplify_via_anf(reducedSurvivalConstraint);
-	reducedSurvivalConstraint = convert_to_cnf(reducedSurvivalConstraint);
-	if (!reducedSurvivalConstraint) {
-		fprintf(stderr, "can't convert reduced survival constraint to CNF\n");
-		return;
-	}
-
-	IRExpr *verificationCondition = simplify_via_anf(summary->verificationCondition);
+	verificationCondition = simplify_via_anf(verificationCondition);
 	verificationCondition = convert_to_cnf(verificationCondition);
 	if (!verificationCondition) {
 		fprintf(stderr, "can't convert verification constraint to CNF\n");
-		return;
+		return verificationCondition;
 	}
 
-	internIRExprTable intern;
-	reducedSurvivalConstraint = internIRExpr(reducedSurvivalConstraint, intern);
 	verificationCondition = internIRExpr(verificationCondition, intern);
-
 	if (verificationCondition->tag != Iex_Associative ||
 	    ((IRExprAssociative *)verificationCondition)->op != Iop_And1)
 		verificationCondition = IRExpr_Associative(Iop_And1, verificationCondition, NULL);
+
+	/* First rule: we only want to keep clauses which interfere
+	   with the the target variables in some sense. */
 	int nr_verification_clauses = ((IRExprAssociative *)verificationCondition)->nr_arguments;
 	IRExpr **verification_clauses = ((IRExprAssociative *)verificationCondition)->contents;
 	bool precious[nr_verification_clauses];
 	for (int i = 0; i < nr_verification_clauses; i++)
 		precious[i] = false;
-
 	std::set<threadAndRegister, threadAndRegister::fullCompare> preciousVariables;
-	enumRegisters(reducedSurvivalConstraint, &preciousVariables);
+	preciousVariables = targetRegisters;
 	int nr_kept = 0;
 	bool progress;
 	progress = true;
@@ -200,18 +180,53 @@ removeRedundantClauses(CrashSummary *summary, OracleInterface *oracle)
 #endif
 
 	if (nr_kept == 0) {
-		summary->verificationCondition = IRExpr_Const(IRConst_U1(1));
+		return IRExpr_Const(IRConst_U1(1));
 	} else if (nr_kept == 1) {
 		for (int i = 0; i < nr_verification_clauses; i++)
 			if (precious[i])
-				summary->verificationCondition = verification_clauses[i];
+				return verification_clauses[i];
+		abort();
 	} else if (nr_kept != nr_verification_clauses) {
 		IRExprAssociative *k = IRExpr_Associative(nr_kept, Iop_And1);
 		for (int i = 0; i < nr_verification_clauses; i++)
 			if (precious[i])
 				k->contents[k->nr_arguments++] = verification_clauses[i];
-		summary->verificationCondition = k;
+		return k;
+	} else {
+		return verificationCondition;
 	}
+}
+
+static bool
+findTargetRegisters(CrashSummary *summary,
+		    OracleInterface *oracle,
+		    internIRExprTable &intern,
+		    std::set<threadAndRegister, threadAndRegister::fullCompare> *targetRegisters)
+{
+	IRExpr *reducedSurvivalConstraint =
+		crossProductSurvivalConstraint(
+			summary->loadMachine,
+			summary->storeMachine,
+			oracle,
+			IRExpr_Const(IRConst_U1(1)),
+			AllowableOptimisations::defaultOptimisations,
+			ALLOW_GC);
+	if (!reducedSurvivalConstraint) {
+		fprintf(stderr, "can't build cross product survival constraint\n");
+		return false;
+	}
+
+	reducedSurvivalConstraint = simplify_via_anf(reducedSurvivalConstraint);
+	reducedSurvivalConstraint = convert_to_cnf(reducedSurvivalConstraint);
+	if (!reducedSurvivalConstraint) {
+		fprintf(stderr, "can't convert reduced survival constraint to CNF\n");
+		return false;
+	}
+	reducedSurvivalConstraint = internIRExpr(reducedSurvivalConstraint, intern);
+
+	enumRegisters(reducedSurvivalConstraint, targetRegisters);
+
+	return true;
 }
 
 int
@@ -228,7 +243,14 @@ main(int argc, char *argv[])
 	summary = readBugReport(argv[1], &first_line);
 	OracleInterface *oracle = new DummyOracle(summary);
 
-	removeRedundantClauses(summary, oracle);
+	internIRExprTable intern;
+	std::set<threadAndRegister, threadAndRegister::fullCompare> targetRegisters;
+	if (findTargetRegisters(summary, oracle, intern, &targetRegisters)) {
+		summary->verificationCondition =
+			removeRedundantClauses(summary->verificationCondition,
+					       intern,
+					       targetRegisters);
+	}
 
 	if (TIMEOUT)
 		fprintf(stderr, "timeout processing %s\n", argv[1]);
