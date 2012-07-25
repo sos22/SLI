3b44ddbeddd628e6caed2bbc19a8413a7c19fd2a
Steven Smith <sos22@cam.ac.uk>
2012-07-25 09:13:13 +0100

    Don't stash load expressions.  We'll instead evaluate them as soon as the arguments are available.

--- sli/enforce_crash/enumNeededExpressions.cpp
+++ sli/enforce_crash/enumNeededExpressions.cpp
@@ -12,20 +12,8 @@ public:
 		out.insert(currentIRExpr());
 		return NULL;
 	}
-	IRExpr *transformIex(IRExprLoad *) {
-		out.insert(currentIRExpr());
-		/* Note that we don't recurse into the address
-		   calculation here.  We can always evaluate this
-		   expression after seeing the load itself, regardless
-		   of where the address came from. */
-		return NULL;
-	}
 	IRExpr *transformIex(IRExprHappensBefore *) {
 		out.insert(currentIRExpr());
-		/* Again, we don't recurse into the details of the
-		   happens before expression, because we only need the
-		   two instructions, and not details of their
-		   side-effects. */
 		return NULL;
 	}
 };
