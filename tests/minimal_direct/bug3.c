5c50079ca97cbd0e82575d3a91abc22538bbe6d0
Steven Smith <sos22@cam.ac.uk>
2012-07-27 09:59:47 +0100

    Add another test case which is like bug2, except that the read operations are much more frequent that the write ones.  This doesn't quite work yet.

---
+++ tests/minimal_direct/bug3.c
@@ -0,0 +1,61 @@
+#include <pthread.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+#define NR_PTRS 100
+static int *volatile global_ptrs[NR_PTRS];
+
+static volatile bool force_quit;
+
+#define STOP_ANALYSIS()					\
+	do {						\
+		int cntr;				\
+		for (cntr = 0; cntr < 1000; cntr++)	\
+			asm ("nop");			\
+	} while (0)
+
+static void *
+thr_main(void *ign)
+{
+	while (!force_quit) {
+		int idx = random() % NR_PTRS;
+		int *p;
+		STOP_ANALYSIS();
+		p = global_ptrs[idx];
+		if (p) {
+			p = global_ptrs[idx];
+			*p = 5;
+		}
+		STOP_ANALYSIS();
+	}
+	return NULL;
+}
+
+int
+main()
+{
+	pthread_t thr;
+	time_t start_time = time(NULL);
+	int t;
+	int idx;
+	pthread_create(&thr, NULL, thr_main, NULL);
+	for (idx = 0; idx < NR_PTRS; idx++)
+		global_ptrs[idx] = &t;
+
+	while (time(NULL) < start_time + 10) {
+		idx = random() % NR_PTRS;
+		STOP_ANALYSIS();
+		global_ptrs[idx] = NULL;
+		STOP_ANALYSIS();
+		global_ptrs[idx] = &t;
+		STOP_ANALYSIS();
+		sleep(1);
+	}
+
+	force_quit = true;
+	pthread_join(thr, NULL);
+	return 0;
+}
