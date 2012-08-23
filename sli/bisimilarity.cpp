0057a405fdb294bf197278195f25d6e75c344a8b
Steven Smith <sos22@cam.ac.uk>
2012-08-23 09:26:24 +0100

    Sanify hash table implementation a bit.

--- sli/bisimilarity.cpp
+++ sli/bisimilarity.cpp
@@ -23,9 +23,11 @@ public:
 	}
 };
 
-template <typename member> class HashedSet {
-	static const int nr_heads = 2053;
-	static const int nr_per_elem = 4;
+template <typename member, int _nr_heads = 2053, int _nr_per_elem = 4> class HashTable {
+public:
+	static const int nr_heads = _nr_heads;
+	static const int nr_per_elem = _nr_per_elem;
+
 	struct elem {
 		struct elem *next;
 		unsigned long use_map;
@@ -35,77 +37,33 @@ template <typename member> class HashedSet {
 		{}
 	};
 	struct elem heads[nr_heads];
-#ifndef NDEBUG
-	std::set<member> std_set;
-#endif
-public:
-	bool contains(const member &v) const {
-		unsigned long h = v.hash() % nr_heads;
-		const struct elem *c = &heads[h];
-		while (c) {
-			for (int i = 0; i < nr_per_elem; i++) {
-				if ( (c->use_map & (1ul << i)) &&
-				     c->content[i] == v ) {
-#ifndef NDEBUG
-					assert(std_set.count(v));
-#endif
-					return true;
-				}
+	~HashTable() {
+		for (int x = 0; x < nr_heads; x++) {
+			struct elem *n;
+			for (struct elem *e = heads[x].next; e; e = n) {
+				n = e->next;
+				delete e;
 			}
-			c = c->next;
 		}
-#ifndef NDEBUG
-		assert(!std_set.count(v));
-#endif
-		return false;
 	}
-	bool insert(const member &v) {
-		unsigned long h = v.hash() % nr_heads;
-		struct elem *c = &heads[h];
-		struct elem **last;
-		member *slot = NULL;
-		unsigned long *slot_use;
-		unsigned long slot_use_mask;
-		while (c) {
-			for (int i = 0; i < nr_per_elem; i++) {
-				if ( (c->use_map & (1ul << i)) ) {
-					if (c->content[i] == v ) {
-#ifndef NDEBUG
-						assert(std_set.count(v));
-#endif
-						return true;
-					}
-				} else if (!slot) {
-					slot = &c->content[i];
-					slot_use = &c->use_map;
-					slot_use_mask = 1ul << i;
-				}
-			}
-			last = &c->next;
-			c = c->next;
-		}
-#ifndef NDEBUG
-		assert(!std_set.count(v));
-		std_set.insert(v);
-#endif
-		if (slot) {
-			*slot = v;
-			*slot_use |= slot_use_mask;
-			return false;
-		}
-		c = new elem();
-		c->use_map = 1;
-		c->content[0] = v;
-		*last = c;
-		return false;
+	const struct elem *getHead(unsigned long h) const {
+		while (h >= (unsigned long)nr_heads)
+			h = (h % nr_heads) ^ (h / nr_heads);
+		return &heads[h];
 	}
+	struct elem *getHead(unsigned long h) {
+		while (h >= (unsigned long)nr_heads)
+			h = (h % nr_heads) ^ (h / nr_heads);
+		return &heads[h];
+	}
+
 	class iterator {
-		HashedSet *owner;
+		HashTable *owner;
 		elem *elm;
 		int idx1;
 		int idx2;
 	public:
-		iterator(HashedSet *_owner)
+		iterator(HashTable *_owner)
 			: owner(_owner)
 		{
 			elm = NULL;
@@ -147,82 +105,89 @@ public:
 		}
 		void erase() {
 			elm->use_map &= ~(1ul << idx1);
-#ifndef NDEBUG
-			assert(owner->std_set.count(elm->content[idx1]));
-			owner->std_set.erase(elm->content[idx1]);
-#endif
 			advance();
 		}
 	};
-	iterator begin() { return iterator(this); }
-	~HashedSet() {
-		for (int x = 0; x < nr_heads; x++) {
-			struct elem *n;
-			for (struct elem *e = heads[x].next; e; e = n) {
-				n = e->next;
-				delete e;
-			}
-		}
-	}
 };
 
-template <typename key, typename value> class HashedMap {
-	static const int nr_heads = 2053;
-	static const int nr_per_elem = 4;
-	struct elem {
-		struct elem *next;
-		unsigned long use_map;
-		std::pair<key, value> content[nr_per_elem];
-		elem()
-			: next(NULL), use_map(0)
-		{}
-	};
-	struct elem heads[nr_heads];
-#ifndef NDEBUG
-	std::map<key, value> std_map;
-#endif
-
+template <typename member> class HashedSet {
+	typedef HashTable<member> contentT;
+	contentT content;
 public:
-	Maybe<value> get(const key &k) const {
-		unsigned long h = k.hash() % nr_heads;
-		const struct elem *c = &heads[h];
+	bool contains(const member &v) const {
+		const typename contentT::elem *c = content.getHead(v.hash());
 		while (c) {
-			for (int i = 0; i < nr_per_elem; i++) {
+			for (int i = 0; i < contentT::nr_per_elem; i++) {
 				if ( (c->use_map & (1ul << i)) &&
-				     c->content[i].first == k ) {
-#ifndef NDEBUG
-					auto it = std_map.find(k);
-					assert(it != std_map.end());
-					assert(it->second == c->content[i].second);
-#endif
-					return Maybe<value>::just(c->content[i].second);
-				}
+				     c->content[i] == v )
+					return true;
 			}
 			c = c->next;
 		}
-#ifndef NDEBUG
-		assert(!std_map.count(k));
-#endif
+		return false;
+	}
+	bool insert(const member &v) {
+		typename contentT::elem *c = content.getHead(v.hash());
+		typename contentT::elem **last;
+		member *slot = NULL;
+		unsigned long *slot_use;
+		unsigned long slot_use_mask;
+		while (c) {
+			for (int i = 0; i < contentT::nr_per_elem; i++) {
+				if ( (c->use_map & (1ul << i)) ) {
+					if (c->content[i] == v )
+						return true;
+				} else if (!slot) {
+					slot = &c->content[i];
+					slot_use = &c->use_map;
+					slot_use_mask = 1ul << i;
+				}
+			}
+			last = &c->next;
+			c = c->next;
+		}
+		if (slot) {
+			*slot = v;
+			*slot_use |= slot_use_mask;
+			return false;
+		}
+		c = new typename contentT::elem();
+		c->use_map = 1;
+		c->content[0] = v;
+		*last = c;
+		return false;
+	}
+	typedef typename contentT::iterator iterator;
+	iterator begin() { return iterator(&content); }
+};
+
+template <typename key, typename value> class HashedMap {
+	typedef HashTable<std::pair<key, value> > contentT;
+	contentT content;
+public:
+	Maybe<value> get(const key &k) const {
+		const typename contentT::elem *c = content.getHead(k.hash());
+		while (c) {
+			for (int i = 0; i < contentT::nr_per_elem; i++) {
+				if ( (c->use_map & (1ul << i)) &&
+				     c->content[i].first == k )
+					return Maybe<value>::just(c->content[i].second);
+			}
+			c = c->next;
+		}
 		return Maybe<value>::nothing();
 	}
 	void set(const key &k, value &v) {
-		unsigned long h = k.hash() % nr_heads;
-		struct elem *c = &heads[h];
-		struct elem **last;
+		typename contentT::elem *c = content.getHead(k.hash());
+		typename contentT::elem **last;
 		std::pair<key, value> *slot = NULL;
 		unsigned long *slot_use;
 		unsigned long slot_use_mask;
 		while (c) {
-			for (int i = 0; i < nr_per_elem; i++) {
+			for (int i = 0; i < contentT::nr_per_elem; i++) {
 				if ( (c->use_map & (1ul << i)) ) {
 					if (c->content[i].first == k ) {
-#ifndef NDEBUG
-						assert(std_map[k] == c->content[i].second);
-#endif
 						c->content[i].second = v;
-#ifndef NDEBUG
-						std_map[k] = v;
-#endif
 						return;
 					}
 				} else if (!slot) {
@@ -234,16 +199,12 @@ public:
 			last = &c->next;
 			c = c->next;
 		}
-#ifndef NDEBUG
-		assert(!std_map.count(k));
-		std_map[k] = v;
-#endif
 		if (slot) {
 			*slot = std::pair<key, value>(k, v);
 			*slot_use |= slot_use_mask;
 			return;
 		}
-		c = new elem();
+		c = new typename contentT::elem();
 		c->use_map = 1;
 		c->content[0].first = k;
 		c->content[0].second = v;
