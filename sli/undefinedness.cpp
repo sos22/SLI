4791ec08ec06d106f580219db333373b746b2e0d
Steven Smith <sos22@cam.ac.uk>
2012-08-23 09:06:33 +0100

    Fix silly typo.

--- sli/undefinedness.cpp
+++ sli/undefinedness.cpp
@@ -259,7 +259,7 @@ undefinednessExpression(StateMachineState *sm, IRExpr *a, const VariableDefinedn
 		if (i->op == Iop_And1 ||
 		    (i->op >= Iop_And8 && i->op <= Iop_And64))
 			return IRExpr_Const(IRConst_U1(0));
-		else if (i->op == Iop_Or1 |\
+		else if (i->op == Iop_Or1 ||
 			 (i->op >= Iop_Or8 && i->op <= Iop_Or64))
 			return IRExpr_Const(IRConst_U1(1));
 		else if ((i->op >= Iop_Add8 && i->op <= Iop_Add64) ||
