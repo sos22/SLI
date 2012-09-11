b52ae9878309df20b7b9abb6a60861ef9ffdbcaf
Steven Smith <sos22@cam.ac.uk>
2012-09-11 09:30:02 +0100

    Start work on a tool to validate that the alias database is reasonably close to being correct.

---
+++ sli/dump_alias_db.cpp
@@ -0,0 +1,238 @@
+#include <sys/types.h>
+#include <sys/fcntl.h>
+#include <sys/mman.h>
+#include <assert.h>
+#include <err.h>
+#include <errno.h>
+#include <math.h>
+#include <sqlite3.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define ROUND_UP_PAGE(x) (((x) + 4095ul) & ~4095ul)
+
+#define MAGIC 0xbb8d4fde1557ab1ful
+
+typedef unsigned char uint8_t;
+
+#define NR_HEADS 524243
+
+struct hash_entry {
+	uint8_t aliases[16];
+	uint8_t stackHasLeaked;
+};
+
+struct header {
+	unsigned long magic;
+	struct {
+		off_t offset;
+		int nr_slots;
+	} heads[NR_HEADS];
+};
+
+struct output_file {
+	int fd;
+	size_t allocated;
+	size_t mapping_size;
+	unsigned long mapping;
+};
+
+static int
+hash_rip(unsigned long h)
+{
+	while (h >= NR_HEADS)
+		h = (h / NR_HEADS) ^ (h % NR_HEADS);
+	return h;
+}
+
+static int
+open_output(const char *fname, struct output_file *of)
+{
+	memset(of, 0, sizeof(*of));
+
+	char *tmp_fname;
+	int cntr;
+	int fd;
+	for (cntr = 0; ; cntr++) {
+		tmp_fname = 0;
+		if (asprintf(&tmp_fname, "%s.%d.tmp", fname, cntr) < 0)
+			err(1, "asprintf()");
+		fd = open(tmp_fname, O_RDWR | O_CREAT | O_EXCL, 0600);
+		if (fd >= 0)
+			break;
+		if (errno != EEXIST) {
+			free(tmp_fname);
+			return -1;
+		}
+		free(tmp_fname);
+	}
+	size_t sz = sizeof(header);
+	void *mapping;
+	sz = ROUND_UP_PAGE(sz);
+	if (ftruncate(fd, sz) < 0)
+		goto failed;
+	mapping = mmap(NULL, sz, PROT_READ|PROT_WRITE,
+		       MAP_SHARED, fd, 0);
+	if (mapping == MAP_FAILED)
+		goto failed;
+	if (rename(tmp_fname, fname) < 0) {
+		munmap(mapping, sz);
+		goto failed;
+	}
+
+	of->fd = fd;
+	of->allocated = sizeof(header);
+	of->mapping_size = sz;
+	of->mapping = (unsigned long)mapping;
+
+	/* We deliberately don't set the magic number here; that's
+	   only done once the rest of the file is finished. */
+	return 0;
+
+failed:
+	close(fd);
+	unlink(tmp_fname);
+	free(tmp_fname);
+	return -1;
+}
+
+static void
+allocate_hash_chain(struct output_file *f, int idx, int len)
+{
+	assert(len > 0);
+	struct header *h = (struct header *)f->mapping;
+	h->heads[idx].offset = f->allocated;
+	unsigned needed = (sizeof(struct hash_entry) + 8) * len;
+	needed = (needed + 7) & ~7u;
+	f->allocated += needed;
+}
+
+static void
+add_entry(struct output_file *f, int idx, int max_this_slot, unsigned long rip, int stackHasLeaked, const uint8_t *alias)
+{
+	assert(max_this_slot > 0);
+	if (f->allocated > f->mapping_size) {
+		if (ftruncate(f->fd, f->allocated) < 0)
+			err(1, "expanding output file to %zd bytes", f->allocated);
+		void *nmap = mmap(NULL, ROUND_UP_PAGE(f->allocated), PROT_READ|PROT_WRITE,
+				  MAP_SHARED, f->fd, 0);
+		if (nmap == MAP_FAILED)
+			err(1, "re-mapping expanded output file");
+		munmap((void *)f->mapping, f->mapping_size);
+		f->mapping_size = ROUND_UP_PAGE(f->allocated);
+		f->mapping = (unsigned long)nmap;
+	}
+
+	struct header *h = (struct header *)f->mapping;
+	assert(h->heads[idx].offset != 0);
+	assert(h->heads[idx].offset >= (off_t)sizeof(header));
+	assert(h->heads[idx].offset + sizeof(struct hash_entry) * max_this_slot <= f->allocated);
+	assert(h->heads[idx].nr_slots < max_this_slot);
+	unsigned long *rip_area = (unsigned long *)(f->mapping + h->heads[idx].offset);
+	struct hash_entry *entries = (struct hash_entry *)(f->mapping + h->heads[idx].offset + 8 * max_this_slot);
+	struct hash_entry *he = &entries[h->heads[idx].nr_slots];
+	rip_area[h->heads[idx].nr_slots] = rip;
+	memcpy(he->aliases, alias, 16);
+	he->stackHasLeaked = stackHasLeaked;
+	h->heads[idx].nr_slots++;
+}
+
+static void
+flush_output_file(struct output_file *f)
+{
+	struct header *h = (struct header *)f->mapping;
+	h->magic = MAGIC;
+}
+
+int
+main(int argc, char *argv[])
+{
+	if (argc != 3)
+		errx(1, "expected two arguments, an input DB and an out put file");
+	const char *db_fname = argv[1];
+	const char *output_fname = argv[2];
+	sqlite3 *database = NULL;
+	int rc = sqlite3_open_v2(db_fname, &database, SQLITE_OPEN_READONLY, NULL);
+	if (rc != SQLITE_OK) {
+		if (database)
+			errx(1, "opening %s: %s", db_fname, sqlite3_errmsg(database));
+		else
+			errx(1, "opening %s: %d", db_fname, rc);
+	}
+
+	struct output_file output;
+	if (open_output(output_fname, &output) < 0)
+		err(1, "opening %s", output_fname);
+
+	/* First, figure out how long all of the hash chains are going to be. */
+	sqlite3_stmt *stmt;
+	if (sqlite3_prepare_v2(
+		    database,
+		    "SELECT rip FROM instructionAttributes",
+		    -1,
+		    &stmt,
+		    NULL) != SQLITE_OK)
+		errx(1, "preparing first SQL statement: %s", sqlite3_errmsg(database));
+	int hash_chain_lengths[NR_HEADS];
+	int tot_nr_entries = 0;
+	memset(hash_chain_lengths, 0, sizeof(hash_chain_lengths));
+	while ((rc = sqlite3_step(stmt)) == SQLITE_ROW) {
+		unsigned long rip = sqlite3_column_int64(stmt, 0);
+		if (rip == 0)
+			errx(1, "RIP of 0 in the database?");
+		int h = hash_rip(rip);
+		hash_chain_lengths[h]++;
+		tot_nr_entries++;
+	}
+	if (rc != SQLITE_DONE)
+		errx(1, "determining hash table shape: %s", sqlite3_errmsg(database));
+
+	/* And some simple statistics... */
+	int longest_length = -1;
+	long len2 = 0;
+	int nr_empty = 0;
+	for (int i = 0; i < NR_HEADS; i++) {
+		if (hash_chain_lengths[i] > longest_length)
+			longest_length = hash_chain_lengths[i];
+		len2 += hash_chain_lengths[i] * hash_chain_lengths[i];
+		if (hash_chain_lengths[i] == 0)
+			nr_empty++;
+	}
+	printf("Average chain length %f, longest %d, average2 length %f, %f%% empty\n",
+	       (double)tot_nr_entries / NR_HEADS,
+	       longest_length,
+	       sqrt(len2 / (double)NR_HEADS),
+	       nr_empty * 100.0 / NR_HEADS);
+
+	/* Now allocate space in the file */
+	for (int i = 0; i < NR_HEADS; i++) {
+		if (hash_chain_lengths[i] != 0)
+			allocate_hash_chain(&output, i, hash_chain_lengths[i]);
+	}
+
+	/* Now we can actually populate the file. */
+	if (sqlite3_prepare_v2(
+		    database,
+		    "SELECT rip, alias0, alias1, alias2, alias3, alias4, alias5, alias6, alias7, alias8, alias9, alias10, alias11, alias12, alias13, alias14, alias15, stackHasLeaked FROM instructionAttributes",
+		    -1,
+		    &stmt,
+		    NULL) != SQLITE_OK)
+		errx(1, "preparing SQL statement: %s", sqlite3_errmsg(database));
+
+	while ((rc = sqlite3_step(stmt)) == SQLITE_ROW) {
+		unsigned long rip = sqlite3_column_int64(stmt, 0);
+		uint8_t alias[16];
+		for (int i = 0; i < 16; i++)
+			alias[i] = sqlite3_column_int(stmt, i + 1);
+		int stackHasLeaked = sqlite3_column_int(stmt, 17);
+		int idx = hash_rip(rip);
+		add_entry(&output, idx, hash_chain_lengths[idx], rip, stackHasLeaked, alias);
+	}
+	if (rc != SQLITE_DONE)
+		errx(1, "advancing SQL query: %s", sqlite3_errmsg(database));
+	flush_output_file(&output);
+
+	return 0;
+}
--- valgrind-3.8.0/sca/sca_main.c
+++ valgrind-3.8.0/sca/sca_main.c
@@ -4,67 +4,214 @@
 /*--------------------------------------------------------------------*/
 
 /*
-   This file is part of Nulgrind, the minimal Valgrind tool,
-   which does no instrumentation or analysis.
+  This file is part of Nulgrind, the minimal Valgrind tool,
+  which does no instrumentation or analysis.
 
-   Copyright (C) 2002-2012 Nicholas Nethercote
-      njn@valgrind.org
+  Copyright (C) 2002-2012 Nicholas Nethercote
+  njn@valgrind.org
 
-   This program is free software; you can redistribute it and/or
-   modify it under the terms of the GNU General Public License as
-   published by the Free Software Foundation; either version 2 of the
-   License, or (at your option) any later version.
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License as
+  published by the Free Software Foundation; either version 2 of the
+  License, or (at your option) any later version.
 
-   This program is distributed in the hope that it will be useful, but
-   WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   General Public License for more details.
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
 
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
-   02111-1307, USA.
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+  02111-1307, USA.
 
-   The GNU General Public License is contained in the file COPYING.
+  The GNU General Public License is contained in the file COPYING.
 */
 
 #include "pub_tool_basics.h"
 #include "pub_tool_tooliface.h"
+#include "pub_tool_vki.h"
+#include "pub_tool_libcbase.h"
+#include "pub_tool_libcfile.h"
+#include "pub_tool_libcprint.h"
+#include "pub_tool_libcassert.h"
+#include "pub_tool_options.h"
+#include "pub_tool_aspacemgr.h"
+#include "vki/vki-scnums-amd64-linux.h"
+#include "libvex_emwarn.h"
+#include "libvex_guest_amd64.h"
+
+static Long sca_db_size;
+static unsigned long database;
+
+#define DATABASE_MAGIC 0xbb8d4fde1557ab1ful
+#define NR_HEADS 524243
+
+struct hash_head {
+	unsigned long offset;
+	int nr_slots;
+};
+struct hash_entry {
+	unsigned char aliases[16];
+	unsigned char stackHasLeaked;
+};
 
 static void sca_post_clo_init(void)
 {
+	if (database == 0)
+		VG_(tool_panic)("No database specified\n");
 }
 
-static
-IRSB* sca_instrument ( VgCallbackClosure* closure,
-                      IRSB* bb,
-                      VexGuestLayout* layout, 
-                      VexGuestExtents* vge,
-                      IRType gWordTy, IRType hWordTy )
+static int
+hash_rip(unsigned long h)
 {
-    return bb;
+	while (h >= NR_HEADS)
+		h = (h / NR_HEADS) ^ (h % NR_HEADS);
+	return h;
+}
+
+static VexEmWarn
+sca_instr_cb(VexGuestAMD64State* vex_state)
+{
+	int h = hash_rip(vex_state->guest_RIP);
+	const struct hash_head *hh = (const struct hash_head *)(database + 8 + 16 * h);
+	int nr_slots = hh->nr_slots;
+	const unsigned long *rip_area = (const unsigned long *)(database + hh->offset);
+	int i;
+	for (i = 0; i < nr_slots; i++) {
+		if (rip_area[i] == 0)
+			VG_(tool_panic)("Corrupt database\n");
+		if (rip_area[i] == vex_state->guest_RIP)
+			break;
+	}
+	if (i == nr_slots) {
+		const NSegment *seg = VG_(am_find_nsegment)(vex_state->guest_RIP);
+		HChar *n;
+		n = VG_(am_get_filename)(seg);
+		if (n && VG_(strncmp)(n, "/lib/", 5)) {
+			VG_(printf)("WARNING: No database entry for RIP %llx (%s)\n", vex_state->guest_RIP, n);
+		}
+		return EmWarn_NONE;
+	}
+//	VG_(printf)("Found database entry for RIP %llx (%d, %d)\n", vex_state->guest_RIP, i, h);
+	return EmWarn_NONE;
+}
+
+static IRSB *
+sca_instrument ( VgCallbackClosure* closure,
+		 IRSB* sbIn,
+		 VexGuestLayout* layout, 
+		 VexGuestExtents* vge,
+		 IRType gWordTy, IRType hWordTy )
+{
+	IRSB *sbOut = deepCopyIRSBExceptStmts(sbIn);
+	int i;
+	IRCallee *cee;
+	IRDirty *d;
+	IRStmt *stmt;
+	cee = mkIRCallee(0, "sca_instr", sca_instr_cb);
+	d = emptyIRDirty();
+	d->cee = cee;
+	d->guard = IRExpr_Const(IRConst_U1(1));
+	d->args = mkIRExprVec_0();
+	d->tmp = IRTemp_INVALID;
+	d->mFx = Ifx_None;
+	d->mAddr = NULL;
+	d->mSize = 0;
+	d->needsBBP = True;
+	d->nFxState = 1;
+	d->fxState[0].fx = Ifx_Read;
+	d->fxState[0].offset = 0;
+	d->fxState[0].size = sizeof(VexGuestAMD64State);
+	d->fxState[0].nRepeats = 0;
+	d->fxState[0].repeatLen = 0;
+	stmt = IRStmt_Dirty(d);
+	for (i = 0; i < sbIn->stmts_used; i++) {
+		addStmtToIRSB(sbOut, sbIn->stmts[i]);
+		if (sbIn->stmts[i]->tag == Ist_IMark)
+			addStmtToIRSB(sbOut, stmt);
+	}
+	return sbOut;
 }
 
 static void sca_fini(Int exitcode)
 {
 }
 
-static void sca_pre_clo_init(void)
+static Bool
+sca_process_command_line_option(Char *opt)
 {
-   VG_(details_name)            ("Nulgrind");
-   VG_(details_version)         (NULL);
-   VG_(details_description)     ("the minimal Valgrind tool");
-   VG_(details_copyright_author)(
-      "Copyright (C) 2002-2012, and GNU GPL'd, by Nicholas Nethercote.");
-   VG_(details_bug_reports_to)  (VG_BUGS_TO);
+	Char *pathname;
+	if (VG_STR_CLO(opt, "--database", pathname)) {
+		int fd = VG_(fd_open)(pathname, VKI_O_RDONLY, 0);
+		struct vg_stat s;
+		unsigned long mmap_addr;
+		register unsigned long r10 asm ("r10");
+		register unsigned long r8 asm("r8");
+		register unsigned long r9 asm("r9");
+		if (fd < 0) {
+			VG_(printf)("Cannot open %s\n", pathname);
+			VG_(tool_panic)("Cannot open database");
+		}
+		if (VG_(fstat)(fd, &s) < 0)
+			VG_(tool_panic)("Cannot stat database");
+		sca_db_size = s.size;
 
-   VG_(details_avg_translation_sizeB) ( 275 );
+		r10 = VKI_MAP_PRIVATE;
+		r8 = fd;
+		r9 = 0;
+		asm ("syscall\n"
+		     : "=a" (mmap_addr)
+		     : "0" (__NR_mmap),
+		       "D" (0),
+		       "S" ((s.size + 4095ul) & ~4095ul),
+		       "d" (VKI_PROT_READ),
+		       "r" (r10),
+		       "r" (r8),
+		       "r" (r9)
+		     : "memory", "flags");
+		if ((long)mmap_addr < 0)
+			VG_(tool_panic)("Cannot mmap database");
+		if (*(unsigned long *)mmap_addr != DATABASE_MAGIC)
+			VG_(tool_panic)("Database contains bad magic number");
+		database = mmap_addr;
+		VG_(close)(fd);
+		return True;
+	} else {
+		return False;
+	}
+}
 
-   VG_(basic_tool_funcs)        (sca_post_clo_init,
-                                 sca_instrument,
-                                 sca_fini);
+static void
+sca_print_usage(void)
+{
+	VG_(printf)("Only tool argument is --database=..., the path to the database\n");
+}
 
-   /* No needs, no core events to track */
+static void
+sca_print_debug_usage(void)
+{
+}
+
+static void
+sca_pre_clo_init(void)
+{
+	VG_(details_name)            ("SCA");
+	VG_(details_version)         (NULL);
+	VG_(details_description)     ("the minimal Valgrind tool");
+	VG_(details_copyright_author)(
+		"Copyright (C) 2002-2012, and GNU GPL'd, by Nicholas Nethercote.");
+	VG_(details_bug_reports_to)  (VG_BUGS_TO);
+
+	VG_(details_avg_translation_sizeB) ( 275 );
+
+	VG_(basic_tool_funcs)        (sca_post_clo_init,
+				      sca_instrument,
+				      sca_fini);
+
+	VG_(needs_command_line_options) (sca_process_command_line_option,
+					 sca_print_usage,
+					 sca_print_debug_usage);
 }
 
 VG_DETERMINE_INTERFACE_VERSION(sca_pre_clo_init)
