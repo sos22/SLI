a8f00b85ee80dd478c51d3f6d929ed25d81ffe14
Steven Smith <sos22@cam.ac.uk>
2012-09-17 09:20:26 +0100

    Fix the run_minimal_direct_locally script to be a bit less stupid.

--- scripts/run_minimal_direct_locally.sh
+++ scripts/run_minimal_direct_locally.sh
@@ -1,12 +1,20 @@
 #! /bin/sh
 
-start="$1"
-end="$2"
+jobid="$1"
+tag="$2"
+start="$3"
+end="$4"
 
 set -e
 set -x
 
 ulimit -c unlimited
 
-cd /local/scratch/sos22/sli/2012-08-12
-../minimal_direct ./mysqld types-new-format-no-head-canon.dat callgraph-newformat.dat ${start}...${end}
+echo "Arguments $*"
+cd /local/scratch/sos22/sli/${tag}
+mkdir ${jobid}
+cd ${jobid}
+ln -s ../static.db
+mkdir logs
+mkdir crash_summaries
+../../minimal_direct ../mysqld ../types-new-format-no-head-canon.dat ../callgraph-newformat.dat ${start}...${end}
