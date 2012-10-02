4d1df5f6285fc0315fa0e0aad59cb7e2535a52cb
Steven Smith <sos22@cam.ac.uk>
2012-10-02 10:35:43 +0100

    Add another test case which looks at what happens when we combine multiple summaries into a single CED.

---
+++ tests/minimal_direct/bug9.c
@@ -0,0 +1,85 @@
+#include <pthread.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+static int *volatile global_ptr1;
+static int *volatile global_ptr2;
+
+static int read_events[2];
+static int write_events;
+static volatile bool force_quit;
+
+#define BAD_PTR ((void *)0x57ul)
+#define STOP_ANALYSIS()					\
+	asm (".fill 100,1,0x90\n")
+
+static void
+bug1(void)
+{
+	int *p = global_ptr1;
+	if (p != BAD_PTR) {
+		p = global_ptr1;
+		*p = 5;
+	}
+}
+
+static void
+bug2(void)
+{
+	static int q;
+	global_ptr2 = &q;
+	*global_ptr2 = 6;
+}
+
+static void *
+thr_main(void *ign)
+{
+	int r;
+	while (!force_quit) {
+		r = random() % 2;
+		STOP_ANALYSIS();
+		if (r) {
+			bug1();
+		} else {
+			bug2();
+		}
+		STOP_ANALYSIS();
+		read_events[r]++;
+	}
+	return NULL;
+}
+
+int
+main()
+{
+	pthread_t thr;
+	static int t;
+	int cntr;
+
+	global_ptr1 = &t;
+
+	pthread_create(&thr, NULL, thr_main, NULL);
+
+	usleep(100000);
+	time_t start_time = time(NULL);
+
+	while (time(NULL) < start_time + 10) {
+		for (cntr = 0; cntr < 2000000; cntr++) {
+			STOP_ANALYSIS();
+			global_ptr2 = BAD_PTR;
+			STOP_ANALYSIS();
+		}
+		STOP_ANALYSIS();
+		global_ptr1 = &t;
+		STOP_ANALYSIS();
+		global_ptr1 = BAD_PTR;
+		STOP_ANALYSIS();
+		write_events++;
+		usleep(1);
+	}
+
+	printf("%d,%d read events, %d write\n", read_events[0], read_events[1], write_events);
+	return 0;
+}
