e133b38a706b232403f08d060330042d4314ad7b
Steven Smith <sos22@cam.ac.uk>
2012-10-06 15:34:13 +0100

    Make it a bit more obvious what's going on when bug1 survives (now that we have some way of making it survive :) ).

--- tests/minimal_direct/bug1.c
+++ tests/minimal_direct/bug1.c
@@ -3,6 +3,9 @@
 #include <time.h>
 #include <unistd.h>
 
+static int read_cntr;
+static int write_cntr;
+
 #define STOP_ANALYSIS()					\
 	asm (".fill 100,1,0x90\n")
 
@@ -17,6 +20,8 @@ thr_main(void *ign)
 			p = global_ptr;
 			*p = 5;
 		}
+		STOP_ANALYSIS();
+		read_cntr++;
 		STOP_ANALYSIS();
 	}
 	return NULL;
@@ -41,7 +46,9 @@ main()
 		STOP_ANALYSIS();
 		global_ptr = NULL;
 		STOP_ANALYSIS();
+		write_cntr++;
 	}
 
+	printf("%d read events, %d write\n", read_cntr, write_cntr);
 	return 0;
 }
