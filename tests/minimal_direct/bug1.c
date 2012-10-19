00da0d42b6535fb92e3014556e0c01f2ee6963e0
Steven Smith <sos22@cam.ac.uk>
2012-10-19 17:02:16 +0100

    Make sure that the read and write counters in bug1 actually get updated, even in optimised builds.

--- tests/minimal_direct/bug1.c
+++ tests/minimal_direct/bug1.c
@@ -3,8 +3,8 @@
 #include <time.h>
 #include <unistd.h>
 
-static int read_cntr;
-static int write_cntr;
+volatile int read_cntr;
+volatile int write_cntr;
 
 #define STOP_ANALYSIS()					\
 	asm (".fill 100,1,0x90\n")
