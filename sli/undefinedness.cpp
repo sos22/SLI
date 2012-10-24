fdccc8ba20649400b71972196547aa2773d9b49c
Steven Smith <sos22@cam.ac.uk>
2012-10-24 15:36:29 +0100

    Add in a missing case to undefinedness analysis.

--- sli/undefinedness.cpp
+++ sli/undefinedness.cpp
@@ -193,6 +193,7 @@ undefinednessExpression(StateMachineState *sm, IRExpr *a, const VariableDefinedn
 			case Iop_32to8:
 			case Iop_32to16:
 			case Iop_16to8:
+			case Iop_ReinterpF64asI64:
 				return UNDEFINED_EXPR;
 			default:
 				printIRExpr(a);
