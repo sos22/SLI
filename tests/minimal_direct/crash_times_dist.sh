ec1bcf76c2ef47ddabdc92caa12df351d2b49f7d
Steven Smith <sos22@cam.ac.uk>
2012-11-08 13:00:41 +0000

    Better handling of timeouts while collecting crash times.

--- tests/minimal_direct/crash_times_dist.sh
+++ tests/minimal_direct/crash_times_dist.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#! /bin/bash
 
 exe="$1"
 enforcer="$2"
@@ -11,13 +11,22 @@ set -e
 # uniform?
 SOS22_RUN_FOREVER=1 ./md_test_dir/harness -n 20 $harness_args $enforcer $exe > $data
 t=$(mktemp)
-check_dist < $data > $t
+mk_timeouts() {
+    n=$(grep T "$data" | wc -l)
+    if [ $n = 0 ]
+    then
+	return
+    fi
+    echo -n "; $n timeouts"
+}
+timeouts=$(mk_timeouts)
+grep -v "T" $data | check_dist > $t
 if grep -q "Cannot reject normality " "$t"
 then
     mean=$(grep "pop stats" "$t" | sed 's/.*mean = \([-0-9.]*\),.*/\1/')
     sd=$(grep "pop stats" "$t" | sed 's/.*sd = \([-0-9.]*\),.*/\1/')
     rm "$t"
-    echo "\$$mean \\pm $sd\$"
+    echo "\$$mean \\pm $sd\$${timeouts}"
     exit 0
 fi
 if grep -q "Might be uniform" "$t"
@@ -25,10 +34,16 @@ then
     low=$(grep "Might be uniform" "$t" | sed 's/.*uniform(\([0-9.-]*\),.*/\1/')
     high=$(grep "Might be uniform" "$t" | sed 's/.*uniform(\([0-9.-]*\), \([0-9.-]*\)).*/\2/')
     rm "$t"
-    echo "\$\[$low, $high\]\$"
+    echo "\$\[$low, $high\]\$${timeouts}"
     exit 0
 fi
 rm "$t"
+
+if ! [ -z "$timeouts" ]
+then
+    echo "Failed${timeouts}"
+    exit 0
+fi
 
 echo "Not uniform or gaussian, try resampling" >&2
 
@@ -37,15 +52,16 @@ echo "Not uniform or gaussian, try resampling" >&2
 # Need a bit more data for this.
 SOS22_RUN_FOREVER=1 ./md_test_dir/harness -n 80 $harness_args $enforcer $exe >> $data
 t2=$(mktemp)
-./tests/minimal_direct/resample.py < "$data" | head -n 10 > "$t2"
+grep -v T "$data" | ./tests/minimal_direct/resample.py | head -n 10 > "$t2"
 check_dist < "$t2" > "$t"
+timeouts=$(mk_timeouts)
 rm "$t2"
 if grep -q "Cannot reject normality " "$t"
 then
     mean=$(grep "pop stats" "$t" | sed 's/.*mean = \([-0-9.]*\),.*/\1/')
     sd=$(grep "pop stats" "$t" | sed 's/.*sd = \([-0-9.]*\),.*/\1/')
     rm "$t"
-    echo "\$\lambda = $mean \\pm $sd\$"
+    echo "\$\lambda = $mean \\pm $sd\$${timeouts}"
     exit 0
 fi
 rm "$t"
