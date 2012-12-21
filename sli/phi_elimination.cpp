11beef9b5e3520b85c3ad076730a75bd2394c163
Steven Smith <sos22@cam.ac.uk>
2012-12-21 12:27:39 +0000

    Better phi elimination: when the predecessor node is a bifurcate, add the branch condition to the selection condition rather than to the assumption.

--- sli/phi_elimination.cpp
+++ sli/phi_elimination.cpp
@@ -294,19 +294,20 @@ build_selection_bdd(SMScopes *scopes,
 			bool failed = false;
 			for (auto it = pred.begin(); !failed && it != pred.end(); it++) {
 				bbdd *ass = assumption;
+				bbdd *condition = cdg.domOf(*it);
 				if ( (*it)->type == StateMachineState::Bifurcate &&
 				     ((StateMachineBifurcate *)*it)->trueTarget !=
 				     ((StateMachineBifurcate *)*it)->falseTarget ) {
 					if ( ((StateMachineBifurcate *)*it)->trueTarget == state ) {
-						ass = bbdd::And(&scopes->bools, ass,
-								((StateMachineBifurcate *)*it)->condition);
+						condition = bbdd::And(&scopes->bools, condition,
+								      ((StateMachineBifurcate *)*it)->condition);
 					} else {
 						assert(((StateMachineBifurcate *)*it)->falseTarget == state);
-						ass = bbdd::And(&scopes->bools, ass,
-								bbdd::invert(&scopes->bools, ((StateMachineBifurcate *)*it)->condition));
+						condition = bbdd::And(&scopes->bools, condition,
+								      bbdd::invert(&scopes->bools, ((StateMachineBifurcate *)*it)->condition));
 					}
 				}
-				bbdd *condition = bbdd::assume(&scopes->bools, cdg.domOf(*it), ass);
+				condition = bbdd::assume(&scopes->bools, condition, ass);
 				exprbdd *res = exprbdd::assume(&scopes->exprs, m[*it], ass);
 				exprbdd **slot = enabling.getSlot(condition, res);
 				if (*slot != res)
