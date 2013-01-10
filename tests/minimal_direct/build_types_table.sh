4bb7ef6450c15f87d5b0451acffbc1892ba83070
Steven Smith <sos22@cam.ac.uk>
2013-01-10 09:42:23 +0000

    Fix build_types_table.sh for new FT2 interface.

--- tests/minimal_direct/build_types_table.sh
+++ tests/minimal_direct/build_types_table.sh
@@ -2,6 +2,7 @@
 
 valgrind="/local/scratch/sos22/valgrind-ft-stage3/bin/valgrind"
 program="$*"
+typedb="${1/.exe/.types}"
 
 # The test program contains racing bugs, or there wouldn't be any
 # point in using it, so try running it three times before giving up.
@@ -12,8 +13,6 @@ do
 	echo "Cannot run $*!"
 	exit 1
     fi
-    script -c "$valgrind --tool=ft2 $program" my_script && break
+    rm -f "$typedb"
+    $valgrind --tool=ft2 --output="$typedb" "$program" && break
 done
-path=$(grep "^Dumping results to" my_script | sed 's,Dumping results to \(/tmp/types[0-9]*\.dat\).,\1,')
-mv $path ${1/.exe/.types}
-rm -f my_script
