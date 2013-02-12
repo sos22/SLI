d371e8583d5c60f13b902dba554fd6b34c2a015c
Steven Smith <sos22@cam.ac.uk>
2013-02-12 14:41:52 +0000

    Extend BDD quickSimplify a bit more.

--- sli/bdd.cpp
+++ sli/bdd.cpp
@@ -528,6 +528,7 @@ _quickSimplify(IRExpr *a, std::map<IRExpr *, IRExpr *> &memo)
 				unsigned long mod = num % denom;
 				return IRExpr_Const_U128(mod, div);
 			}
+			case Iop_CmpF64:
 			case Iop_F64toI64: {
 				/* It's not worth trying to constant
 				 * fold these ones. */
