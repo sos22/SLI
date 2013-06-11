18328e1325783609bad6bc09b181166061e14dbe
Steven Smith <sos22@cam.ac.uk>
2013-06-11 16:06:46 +0100

    Rework vary_nr_ptrs so that it:
    a) Has a sensible number of repetitions
    b) Tests a sensible set of abscissae
    c) Interleaves testing the with and without enforcer cases.

--- tests/minimal_direct/vary_nr_ptrs.sh
+++ tests/minimal_direct/vary_nr_ptrs.sh
@@ -3,12 +3,11 @@
 set -e
 set -x
 
-nr_reps=2
+nr_reps=110
 nr_discards=0
 
 function abscissae() {
-    #echo 1 10 20 30 40 50 60 70 80 90 100 150 200 250 300 350 400 450 500 750 1000 2000 3000 4000 5000 7500 10000
-    echo 1 10 20
+    echo 1 2 3 4 5 6 7 8 9 10 20 30 40 50 60 70 80 90 100 150 200 250 300 350 400 450 500 750 1000 2000 3000 4000 5000 7500 10000 20000 40000 80000 160000 320000
 }
 function tp() {
     local n="$1"
@@ -62,17 +61,26 @@ for i in `seq 1 $(($nr_reps + $nr_discards))`
 do
     for a in `abscissae`
     do
-	echo $a
+	echo with $a
+	if [ "$a" -lt 4000 ]
+	then
+	    echo without $a
+	fi
     done
 done | ./scripts/shuffle.py > "$sched"
 
-cat "$sched" | while read absc
+cat "$sched" | while read use_interp absc
 do
     p=$(tp $absc)
     t=$(mktemp)
     interp=$(echo ${p}~0.interp.so | sed 's/[:=]/~/g')
-    ! SOS22_ENFORCER_RANDOMISE=1 SOS22_RUN_FOREVER=1 LD_PRELOAD=${interp} ./scripts/sane_time "$t" ${p}.exe
+    if [ "$use_interp" = "with" ]
+    then
+	! SOS22_ENFORCER_RANDOMISE=1 SOS22_RUN_FOREVER=1 LD_PRELOAD=${interp} ./scripts/sane_time "$t" ${p}.exe
+    else
+	! SOS22_RUN_FOREVER=1 ./scripts/sane_time "$t" ${p}.exe
+    fi
     read time < "$t"
     rm -f "$t"
-    echo "$absc $time" >> "$logfile"
+    echo "$use_interp $absc $time" >> "$logfile"
 done
